4de4fb7d034fea6ad3485f36cf33c22b9b1ffbcb
Viren <chudasamaviren08@gmail.com>
2024-09-22 16:53:18 +0530

    update meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bhavya Quartz",
-  description: "Bhavya Quartz",
+  description: "Bhavya Quartz - Your go-to source for quality quartz products",
 };
 
 export default function RootLayout({
@@ -19,7 +19,21 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta name="description" content={metadata.description ?? ""} />
+        <meta
+          name="keywords"
+          content="Quartz Stone manufacturer and exporter of Stone & Quartz, Bhavya Quartz"
+        />
+        <meta name="author" content="Bhavya Quartz" />
+        <meta property="og:title" content={String(metadata.title)} />
+        <meta property="og:description" content={metadata.description ?? ""} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.bhavyaquartz.com/" />
+        <meta
+          property="og:image"
+          content="https://bhavyaquartz.com/images/logo.svg"
         />
       </head>
       <body
